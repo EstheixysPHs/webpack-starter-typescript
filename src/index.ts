8d6bad70a7ae12b72aa9f8d806785c36de5dbf45
Estheixys Peña <pehinojosa72@gmail.com>
2021-02-23 14:47:23 -0400

    Actualizar data en Firestore

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,29 @@ const usuario = {
     fechaNaci: 0
 }
 
+const usuariosRef = db.collection('usuarios');
+
+// insert into usuarios ...
 //db.collection('usuarios')
 //    .add(usuario)
 //    .then( docRef => {
 //        console.log(docRef)
 //    })
 //    .catch(e => console.log('error', e))
+
+//update usuarios set activo = false where ....
+//usuariosRef
+//    .doc('5cFN5xFBXArZ5TeuQlZP')
+//    .update({ 
+//        activo: true
+//    });
+    
+usuariosRef
+    .doc('5cFN5xFBXArZ5TeuQlZP')
+    .set({ 
+        activo: true,
+        edad: 44,
+        casado: true,
+        id: 123
+
+    });
